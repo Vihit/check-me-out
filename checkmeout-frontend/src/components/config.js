709d9e65cb46eae923acdefceca1d1aa5429aa3c
Vihit Shah <vihitshah@Vihits-MacBook-Air.local>
2023-01-21 14:27:00 +0530

    handle clicks behind the audit window

--- checkmeout-frontend/src/components/config.js
+++ checkmeout-frontend/src/components/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  // apiUrl: "http://ec2-35-154-203-107.ap-south-1.compute.amazonaws.com:8081/",
-  apiUrl: "http://localhost:8081/",
+  apiUrl: "http://ec2-35-154-203-107.ap-south-1.compute.amazonaws.com:8081/",
+  // apiUrl: "http://localhost:8081/",
 };
